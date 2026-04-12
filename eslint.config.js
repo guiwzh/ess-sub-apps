7195fd1f161722e8920592085f099970edf07be8
Wangzhe Gui <21210720135@m.fudan.edu.cn>
2026-04-12 21:51:02 +0800

    feat: init ess-sub-apps monorepo with pnpm workspaces and turborepo

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['**/dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [tseslint.configs.recommended, prettierConfig],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+    },
+  },
+)
