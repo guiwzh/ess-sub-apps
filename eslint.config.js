0f48cf6834eb4608e6d59ff7fe9c031963af11dc
Wangzhe Gui <21210720135@m.fudan.edu.cn>
2026-04-17 17:20:26 +0800

    refactor(config): migrate from tseslint.config() to ESLint core defineConfig()

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -6,35 +7,32 @@ import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 import unusedImports from 'eslint-plugin-unused-imports'
 
-export default tseslint.config([
-  { ignores: ['**/dist'] },
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [tseslint.configs.recommended, prettierConfig],
-    languageOptions: {
-      ecmaVersion: 2023,
-      globals: globals.browser,
-      parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': 'error',
-      'react-hooks/set-state-in-effect': 'warn',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-      ],
+export default defineConfig({ ignores: ['**/dist'] }, tseslint.configs.recommended, {
+  files: ['**/*.{ts,tsx}'],
+  extends: [prettierConfig],
+  languageOptions: {
+    ecmaVersion: 2023,
+    globals: globals.browser,
+    parserOptions: {
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-])
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    prettier,
+    'unused-imports': unusedImports,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+    'react-hooks/set-state-in-effect': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+  },
+})
