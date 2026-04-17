eb1465fc71d243da65c431e8181ecedee365e1f4
Wangzhe Gui <21210720135@m.fudan.edu.cn>
2026-04-18 02:25:28 +0800

    build: optimize turbo/eslint/tsconfig configs, add utility scripts
    - turbo.json: add inputs/outputs for build/lint/type-check tasks
    - eslint: remove redundant eslint-plugin-prettier, keep eslint-config-prettier only
    - tsconfig.node.json: extend base config instead of duplicating options
    - package.json: add type-check, format, format:check, clean scripts

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig(globalIgnores(['**/dist']), {
@@ -20,11 +19,7 @@ export default defineConfig(globalIgnores(['**/dist']), {
     ecmaVersion: 2023,
     globals: globals.browser,
   },
-  plugins: {
-    prettier: prettierPlugin,
-  },
   rules: {
-    'prettier/prettier': 'error',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react-hooks/set-state-in-effect': 'warn',
     '@typescript-eslint/no-unused-vars': [
